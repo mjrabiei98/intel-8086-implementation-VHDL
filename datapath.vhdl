0019a36a7e1cc4e56db02e6c4f51212304bcf144
mohammadjavad rabiei <mj.rabiei98@gmail.com>
2024-03-31 23:43:27 +0330

    adding firsy element to data path

--- datapath.vhdl
+++ datapath.vhdl
@@ -5,14 +5,39 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 
+
+-- entity reg is 
+--     generic (register_size : integer := 8);
+--     port (clk, rst, en: in std_logic; d : in std_logic_vector (register_size - 1 downto 0); q : out std_logic_vector (register_size-1 downto 0 ) );
+-- end entity reg;
+
+-- entity TriStateBuffer is
+--     generic(buffer_size : integer := 16);
+--     Port (
+--         data_in  : in  std_logic_vector(buffer_size-1 downto 0);  -- Input data
+--         enable   : in  std_logic;  -- Enable signal for the buffer
+--         data_out : out std_logic_vector(buffer_size-1 downto 0)   -- Output data
+--     );
+
 entity datapath is 
-    port ();
+    port (clk, rst, en_ES, ES_to_bus : in std_logic);
 end entity datapath;
 
 architecture bwhavioral of datapath is
 
-    -- signal 
+    signal 
 
 begin
+
+    ES : entity work.reg(behavioral)
+        generic map (16)
+        port map(clk, rst, en_ES, );
+
+    ES_triState : entity work.reg(behavioral)
+        generic map (16)
+        port map( );
+
+        
+
 
 end bwhavioral ; -- bwhavioralsab
